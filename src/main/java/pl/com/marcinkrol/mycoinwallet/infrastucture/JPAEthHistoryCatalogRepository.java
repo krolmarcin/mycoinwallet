dbb2c96f8f5d6744e5b6a7e23b5ac525359c2c68
Marcin <szeryfmk@gmail.com>
2018-08-11 07:41:46 +0200

    pom packaging

--- src/main/java/pl/com/marcinkrol/mycoinwallet/infrastucture/JPAEthHistoryCatalogRepository.java
+++ src/main/java/pl/com/marcinkrol/mycoinwallet/infrastucture/JPAEthHistoryCatalogRepository.java
@@ -2,9 +2,11 @@ package pl.com.marcinkrol.mycoinwallet.infrastucture;
 
 import pl.com.marcinkrol.mycoinwallet.application.EthHistoryCatalog;
 import pl.com.marcinkrol.mycoinwallet.application.EthBalanceHistoryDto;
+import pl.com.marcinkrol.mycoinwallet.domain.AccountAddress;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 public class JPAEthHistoryCatalogRepository implements EthHistoryCatalog {
@@ -12,6 +14,7 @@ public class JPAEthHistoryCatalogRepository implements EthHistoryCatalog {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Override
     public List<EthBalanceHistoryDto> getHistory(String walletId) {
         return null;
     }
